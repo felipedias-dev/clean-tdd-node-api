04ab1fc77bb3f0994c38aef8d60afe28c4d8276e
Felipe Dias <felipedias.dev@gmail.com>
2021-06-23 01:08:27 -0300

    ensure AuthUseCase returns null if an invalid password is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -5,10 +5,12 @@ const makeSut = () => {
   class LoadUserByEmailRepositorySpy {
     async load(email) {
       this.email = email;
+      return this.user;
     }
   }
 
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
+  loadUserByEmailRepositorySpy.user = {};
   const sut = new AuthUseCase(loadUserByEmailRepositorySpy);
 
   return {
@@ -58,10 +60,21 @@ describe('Auth Usecase', () => {
   });
 
   test('Should return null if an invalid email is provided', async () => {
-    const { sut } = makeSut();
+    const { sut, loadUserByEmailRepositorySpy } = makeSut();
+    loadUserByEmailRepositorySpy.user = null;
     const accessToken = await sut.auth(
       'invalid_email@mail.com',
       'any_password'
+    );
+
+    expect(accessToken).toBeNull();
+  });
+
+  test('Should return null if an invalid password is provided', async () => {
+    const { sut } = makeSut();
+    const accessToken = await sut.auth(
+      'any_email@mail.com',
+      'invalid_password'
     );
 
     expect(accessToken).toBeNull();
