e96ec7f9a6ee57686070e0aaa6be6f074c3b092a
Felipe Dias <felipedias.dev@gmail.com>
2021-07-12 02:06:28 -0300

    ensure MongoHelper reconnects when getDb is called and connection is down

--- src/infra/helpers/mongo-helper.spec.js
+++ src/infra/helpers/mongo-helper.spec.js
@@ -1,6 +1,10 @@
 const MongoHelper = require('./mongo-helper');
 
 describe('Mongo Helper', () => {
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
   test('Should reconnect when getDb() is called and connection is down', async () => {
     const sut = MongoHelper;
 
@@ -10,10 +14,7 @@ describe('Mongo Helper', () => {
     await sut.disconnect();
     expect(sut.db).toBeFalsy();
 
-    await sut.getDb();
-    expect(sut.db).toBeTruthy();
-
-    await sut.disconnect();
-    expect(sut.db).toBeFalsy();
+    const db = await sut.getDb();
+    expect(db).toBeTruthy();
   });
 });
