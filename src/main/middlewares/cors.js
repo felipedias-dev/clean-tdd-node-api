1d3425e407d343b83f2e53ccf1b98850178b284c
Felipe Dias <felipedias.dev@gmail.com>
2021-07-18 16:41:23 -0300

    fix request param

--- src/main/middlewares/cors.js
+++ src/main/middlewares/cors.js
@@ -1,4 +1,4 @@
-module.exports = (require, response, next) => {
+module.exports = (request, response, next) => {
   response.set('access-control-allow-origin', '*');
   response.set('access-control-allow-methods', '*');
   response.set('access-control-allow-headers', '*');
